3406ed556c1fe42484f03defe4e5acfddb7f9b47
Sergey Guzun <ser.guzun19@gmail.com>
2020-02-01 23:37:06 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React from "react";
-import TodoItem from './components/todolist'
-import todosData from './components/todosData'
-
-{/* class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 0
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count+1
-      }
-    })
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Изменить!</button>
-      </div>
-    )
-  }
-}*/}
-
+import TodoItem from "./components/todolist";
+import todosData from "./components/todosData";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       todos: todosData
-    }
-    this.handleChange = this.handleChange.bind(this)
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(id) {
-    console.log("Выбрано!", id)
+    console.log("Выбрано!", id);
   }
 
   render() {
-    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} todoitem={item} />)
+    const todoComponents = this.state.todos.map(item => (
+      <TodoItem key={item.id} todoitem={item} />
+    ));
     return (
       <div className="todo-list">
         <h3>Список дел на будни</h3>
         {todoComponents}
       </div>
-    )
+    );
   }
 }
 
-
-export default App
+export default App;
